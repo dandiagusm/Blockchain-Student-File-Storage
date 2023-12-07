8b844018e205729d137cddaf894d22dc8f655557
dandiagusm <d.agusmaulana99@gmail.com>
2023-12-07 14:39:56 +0700

    fix nav

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 export const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
+  displayName: 'Admin SMAN 1 Jonggol',
+  email: 'adminsman1jonggol@gmail.com',
   photoURL: '/assets/images/avatars/avatar_25.jpg',
 };
